6c28f4d29c05679d8234cae4e4480523640db763
Musoftware <mahmoudmn810@gmail.com>
2024-10-24 15:43:26 +0300

    Initial commit

---
+++ MarkdownEditor/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace MarkdownEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBoxMarkdown = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxMarkdown);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1162, 647);
+            this.splitContainer1.SplitterDistance = 552;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // textBoxMarkdown
+            // 
+            this.textBoxMarkdown.BackColor = System.Drawing.Color.DimGray;
+            this.textBoxMarkdown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxMarkdown.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMarkdown.ForeColor = System.Drawing.Color.White;
+            this.textBoxMarkdown.Location = new System.Drawing.Point(0, 0);
+            this.textBoxMarkdown.MaxLength = 1000000000;
+            this.textBoxMarkdown.Multiline = true;
+            this.textBoxMarkdown.Name = "textBoxMarkdown";
+            this.textBoxMarkdown.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxMarkdown.Size = new System.Drawing.Size(552, 647);
+            this.textBoxMarkdown.TabIndex = 0;
+            this.textBoxMarkdown.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(606, 647);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 647);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Markdown Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBoxMarkdown;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ MarkdownEditor/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using HtmlAgilityPack;
+using Markdig;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarkdownEditor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string markdown = textBoxMarkdown.Text;
+            string html = Markdown.ToHtml(markdown);
+            //webBrowser1.DocumentText = html;
+
+            richTextBox1.Rtf = ConvertHtmlToRtf(html);
+        }
+
+
+        private string ConvertHtmlToRtf(string html)
+        {
+            // Load HTML using Html Agility Pack
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            StringBuilder rtfBuilder = new StringBuilder();
+            rtfBuilder.Append(@"{\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang1033");
+
+            foreach (var node in htmlDoc.DocumentNode.ChildNodes)
+            {
+                ProcessNode(node, rtfBuilder);
+            }
+
+            rtfBuilder.Append("}");
+            return rtfBuilder.ToString();
+        }
+
+        private void ProcessNode(HtmlNode node, StringBuilder rtfBuilder)
+        {
+            switch (node.Name.ToLower())
+            {
+                case "h1":
+                    rtfBuilder.Append(@"{\b\fs32 " + node.InnerText + @"\par}");
+                    break;
+                case "h2":
+                    rtfBuilder.Append(@"{\b\fs24 " + node.InnerText + @"\par}");
+                    break;
+                case "h3":
+                    rtfBuilder.Append(@"{\b\fs20 " + node.InnerText + @"\par}");
+                    break;
+                case "p":
+                    rtfBuilder.Append(node.InnerText + @"\par");
+                    break;
+                case "ul":
+                    foreach (var li in node.ChildNodes)
+                    {
+                        if (li.Name == "li")
+                        {
+                            rtfBuilder.Append(@"\bullet " + li.InnerText + @"\par");
+                        }
+                    }
+                    break;
+                case "strong":
+                    rtfBuilder.Append(@"{\b " + node.InnerText + @"}");
+                    break;
+                case "em":
+                    rtfBuilder.Append(@"{\i " + node.InnerText + @"}");
+                    break;
+                case "a":
+                    // For links, convert to clickable format in RTF
+                    rtfBuilder.Append(@"{\ul " + node.InnerText + @"\footnote {\*\footnote {\fldrslt {\*\fldinst HYPERLINK """ + node.GetAttributeValue("href", "") + @"""}}}}");
+                    break;
+                case "img":
+                    // RTF does not support embedding images directly, but you can reference the image
+                    string imgSrc = node.GetAttributeValue("src", "");
+                    rtfBuilder.Append(@"{\pict\wmetafile8\picw" + imgSrc + @"\pich" + imgSrc + @"\picwgoal" + imgSrc + @"\pichgoal" + imgSrc + @"}"); // Placeholder for images
+                    break;
+                case "blockquote":
+                    rtfBuilder.Append(@"{\pard\itap1 " + node.InnerText + @"\par}"); // Indent block quotes
+                    break;
+                // Add more cases as needed
+                default:
+                    foreach (var child in node.ChildNodes)
+                    {
+                        ProcessNode(child, rtfBuilder); // Recursively process child nodes
+                    }
+                    break;
+            }
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
